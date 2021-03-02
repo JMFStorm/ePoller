2e548c667189af3a9fc431db9101874f8d13e68d
Janne Fagerström <jannematiasf@gmail.com>
2021-03-02 16:46:41 +0200

    add heroku api url

--- epoller-client/src/utils/config.ts
+++ epoller-client/src/utils/config.ts
@@ -3,5 +3,5 @@ export let baseUrl: string;
 if (process.env.NODE_ENV === "development") {
   baseUrl = "http://localhost:5000/";
 } else if (process.env.NODE_ENV === "production") {
-  baseUrl = "";
+  baseUrl = "https://epoller-react-app.herokuapp.com/";
 }
